0bf5d6b62019deab1eac282faf9cebdd09eed6e1
minhtriet2104 <minhtriet2104.it@gmail.com>
2020-01-25 13:25:58 +0700

    Updates

---
+++ precache-manifest.a545bf42955147b2e6e581babd607dd6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda1e3ef9188dbccc748663c1fe0669a",
+    "url": "/mt-cv/index.html"
+  },
+  {
+    "revision": "f611a65c48f9e2a065c7",
+    "url": "/mt-cv/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "d3ef7dc309285b30d878",
+    "url": "/mt-cv/static/css/main.deec650d.chunk.css"
+  },
+  {
+    "revision": "f611a65c48f9e2a065c7",
+    "url": "/mt-cv/static/js/2.858d0244.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/mt-cv/static/js/2.858d0244.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3ef7dc309285b30d878",
+    "url": "/mt-cv/static/js/main.3b9d3dfd.chunk.js"
+  },
+  {
+    "revision": "b6edba690af0d54708d5",
+    "url": "/mt-cv/static/js/runtime-main.8a59aedc.js"
+  },
+  {
+    "revision": "aaf74bc9b2fe5749f757e0e888459faa",
+    "url": "/mt-cv/static/media/IMG_9881.aaf74bc9.JPG"
+  }
+]);
