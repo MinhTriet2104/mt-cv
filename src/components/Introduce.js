bc35563be8e02aa1f110b064397f2ed9bc534fc9
minhtriet2104 <minhtriet2104.it@gmail.com>
2020-01-30 12:40:43 +0700

    Change animation Home page

--- src/components/Introduce.js
+++ src/components/Introduce.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { Icon } from "antd";
-import TweenOne from "rc-tween-one";
+import styled, { keyframes } from "styled-components";
+import { tada, rollIn, flipInX } from "react-animations";
 
 import IconContainer from "./containers/IconContainer";
 
 const fontFamily = "Work Sans, sans-serif";
+
+const tadaAnimation = keyframes`${tada}`;
+const rollInAnimation = keyframes`${rollIn}`;
+const flipInXAnimation = keyframes`${flipInX}`;
 
 const GreetingStyle = {
   fontSize: "4rem",
   marginTop: "4rem",
   fontWeight: "bold",
   fontFamily: fontFamily,
-  marginBottom: 0
+  marginBottom: 0,
+  padding: 0
 };
 
 const highlight = {
@@ -20,137 +26,67 @@ const highlight = {
   fontFamily: fontFamily
 };
 
+const Greeting = styled.h1`
+  animation: 1s ${tadaAnimation} ease-in;
+  animation-delay: 0.4s;
+`;
+
+const Job = styled.h2`
+  margin-top: 0;
+  animation: 1.5s ${flipInXAnimation} ease-in;
+`;
+
+const SocialContact = styled.div`
+  animation: 1.2s ${rollInAnimation} ease-in;
+`;
+
 export default () => (
   <div>
-    <TweenOne
-      animation={{
-        y: -100,
-        opacity: 0,
-        scale: 0.2,
-        yoyo: true,
-        duration: 500,
-        repeat: 1
-      }}
-    >
-      <h1 style={GreetingStyle}>
-        Hi, I'm <span style={highlight}>Minh Triết</span>{" "}
-        <Icon type="message" theme="twoTone" twoToneColor="#7aa3ff" />
-      </h1>
-    </TweenOne>
+    <Greeting style={GreetingStyle}>
+      Hi, I'm <span style={highlight}>Minh Triết</span>{" "}
+      <Icon type="message" theme="twoTone" twoToneColor="#7aa3ff" />
+    </Greeting>
 
-    <TweenOne
-      animation={{
-        y: -100,
-        opacity: 0,
-        scale: 0.2,
-        yoyo: true,
-        duration: 500,
-        repeat: 1,
-        delay: 50
-      }}
-    >
-      <h2 style={{ margin: 0 }}>
-        I'm a <span style={highlight}>WEB DESIGN & DEVELOPMENT</span>
-      </h2>
-    </TweenOne>
+    <Job>
+      I'm a <span style={highlight}>WEB DESIGN & DEVELOPMENT</span>
+    </Job>
 
-    <TweenOne
-      animation={{
-        x: -100,
-        opacity: 0,
-        scale: 0.2,
-        yoyo: true,
-        duration: 500,
-        repeat: 1,
-        delay: 100
-      }}
-    >
-      <p style={{ marginTop: "0.5rem", fontWeight: "1.2rem" }}>
-        I can design a website interface, build a complete website manage and
-        develop it. If You want to make a website for anything, you come to the
-        right place! Let me help you to bring your idea come True.
-      </p>
-    </TweenOne>
+    <p style={{ marginTop: "0.5rem", fontWeight: "1.2rem" }}>
+      I can design a website interface, build a complete website manage and
+      develop it. If You want to make a website for anything, you come to the
+      right place! Let me help you to bring your idea come True.
+    </p>
 
     <div style={{ display: "flex", flexDirection: "row" }}>
-      <TweenOne
-        animation={{
-          y: 200,
-          opacity: 0,
-          scale: 0.5,
-          yoyo: true,
-          duration: 500,
-          repeat: 1
-        }}
-      >
+      <SocialContact>
         <IconContainer first={true}>
           <Icon type="facebook" theme="filled" />
         </IconContainer>
-      </TweenOne>
+      </SocialContact>
 
-      <TweenOne
-        animation={{
-          y: 200,
-          opacity: 0,
-          scale: 0.5,
-          yoyo: true,
-          duration: 500,
-          repeat: 1,
-          delay: 50
-        }}
-      >
+      <SocialContact>
         <IconContainer>
           <Icon type="twitter-square" theme="filled" />
         </IconContainer>
-      </TweenOne>
+      </SocialContact>
 
-      <TweenOne
-        animation={{
-          y: 200,
-          opacity: 0,
-          scale: 0.5,
-          yoyo: true,
-          duration: 500,
-          repeat: 1,
-          delay: 100
-        }}
-      >
+      <SocialContact>
         <IconContainer>
           <Icon type="instagram" theme="filled" />
         </IconContainer>
-      </TweenOne>
+      </SocialContact>
 
-      <TweenOne
-        animation={{
-          y: 200,
-          opacity: 0,
-          scale: 0.5,
-          yoyo: true,
-          duration: 500,
-          repeat: 1,
-          delay: 150
-        }}
-      >
+      <SocialContact>
         <IconContainer>
           <Icon type="linkedin" theme="filled" />
         </IconContainer>
-      </TweenOne>
+      </SocialContact>
 
-      <TweenOne
-        animation={{
-          y: 200,
-          opacity: 0,
-          scale: 0.5,
-          yoyo: true,
-          duration: 500,
-          repeat: 1,
-          delay: 200
-        }}
-      >
+      <SocialContact>
         <IconContainer>
           <Icon type="github" theme="filled" />
         </IconContainer>
-      </TweenOne>
+      </SocialContact>
     </div>
   </div>
 );
